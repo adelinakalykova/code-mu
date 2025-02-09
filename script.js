dae7847548f4d9ee346313b7cc8689989988af2c
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-09 16:22:43 +0600

    reduce

--- script.js
+++ script.js
@@ -1,33 +1,8 @@
-//Дано число 12345. Найдите сумму цифр этого числа.
-// let num = "12345";
-// let str = String(num);
-// alert(
-//   Number(str[0]) +
-//     Number(str[1]) +
-//     Number(str[2]) +
-//     Number(str[3]) +
-//     Number(str[4])
-// );
+// Переделайте этот код так, чтобы в нем использовались описанные сокращенные операции:
 
-//Дано число 12345. Найдите произведение цифр этого числа.
-
-// let num = "12345";
-// let str = String(num);
-// alert(
-//   Number(str[0]) *
-//     Number(str[1]) *
-//     Number(str[2]) *
-//     Number(str[3]) *
-//     Number(str[4])
-// );
-
-//Дано число 12345. Переставьте цифры этого числа в обратном порядке.
-let num = 12345;
-let str = num.toString();
-let reversed =
-  str[str.length - 1] +
-  str[str.length - 2] +
-  str[str.length - 3] +
-  str[str.length - 4] +
-  str[str.length - 5];
-alert(reversed);
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);
