215483ae022805d52489a65c21ff8bf723cb3341
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-09 18:10:12 +0600

    index for array

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-//Создайте массив с элементами 1, 2, 3. Выведите на экран каждый из этих элементов.
-// let arr = [1, 2, 3];
-// console.log(arr[0]);
-// console.log(arr[1]);
-// console.log(arr[2]);
-
-//Выведите на экран сумму элементов этого массива.
-// let arr = [1, 2, 3];
-// let sum = 0;
-// for (let i = 0; i < arr.length; i++) {
-//   sum += arr[i];
-// }
-// console.log(sum);
-
-//Дан следующий массив:
-
-let arr = ['a', 'b', 'c', 'd'];
-let result = arr.join('+');
-console.log(result)
-// Выведите с помощью этого массива следующую строку:
-
-// 'a+b+c+d'
+//Создайте массив с произвольными элементами. Выведите на экран количество элементов в этом массиве.
+let arr = ["adelina", "it academy", "lol"];
+console.log(arr.length);
+console.log(arr[arr.length- 1]);
