330424fffab4e9263d4c06780833961548fd0990
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-09 17:28:01 +0600

    find error

--- script.js
+++ script.js
@@ -1,17 +1,65 @@
-// //Выведите в окно браузера какой-нибудь текст.
-// document.write("Adelina Kalykova");
+// 1
+// let num1 = 1;
+// let num2 = 2;
+// console.log('сумма: ' + (num1 + num2));
 
-// //Выведите в окно браузера какой-нибудь текст, сделав его курсивным.
-// document.write("<i>text</i>");
+// 2
+// let a = 1;
+// let b = 2;
+// console.log(a + b);
 
-// Выведите текст из переменной в окно браузера так, чтобы он был курсивным.
+//3
+// let num = '123';
+// let sum = Number(num[0]) + Number(num[1]) + Number(num[2]);
+// console.log(sum);
 
-// let str = "text";
-// document.write("<i>text</i>");
+// //4
+// let num = 123;
+// let numb = String(num);
+// console.log(numb[0]);
 
-//Выведите в окно браузера столбец чисел от 1 до 5
-document.write("1 <br>");
-document.write("2 <br>");
-document.write("3 <br>");
-document.write("4 <br>");
-document.write("5 <br>");
+//5
+// let num = 123;
+// let numb = String(num);
+// console.log(numb.length);
+
+// //6
+// let a = 24 * 60 * 60;
+// console.log(a);
+
+//7
+// let a = 0;
+// console.log(++a);
+
+//8
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+
+//9
+// let num = 123;
+// let str = String(num);
+// console.log(str[str.length -1]);
+
+//10
+// let num = 123;
+// let str = String(num);
+// console.log(str.length);
+
+//11
+// let num = 123;
+// let str = String(num);
+// console.log(str[str.length -1]);
+
+//12
+// let a = '123';
+// let b = '456';
+// let s = Number(a) + Number(b);
+//console.log(s);
+
+//13
+// let aaa = 1;
+// let bbb = 2;
+// let ccc = 3;
+
+// console.log(Number(aaa + bbb + ccc));
