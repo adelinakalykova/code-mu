69d7a2e881b30b707938d8e4b7ccaabc18c8ff71
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-09 19:38:40 +0600

    user object

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-let months = {
-    1: 'January',
-    2: 'February',
-    3: 'March',
-    4: 'April',
-    5: 'May',
-    6: 'June',
-    7: 'July',
-    8: 'August',
-    9: 'September',
-    10: 'October',
-    11: 'November',
-    12: 'December'
-  };
-  
-  console.log(months);  
-  
+let user = {
+  name: "Adelina",
+  surname: "Kalykova",
+  patronymic: "Adylbekovna",
+};
+
+console.log(user.surname + " " + user.name + " " + user.patronymic);
