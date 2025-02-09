2f0423e204f856bfb38e4691b52a1e9fb2a3e2df
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-09 20:34:20 +0600

    primitiv

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// Скажите, что выведется в консоль в результате выполнения следующего кода:
-
-let obj = {x: 1, y: 2, z: 3};
-
-console.log('x' in obj);
-console.log('w' in obj);
+//В JavaScript семь примитивных типов данных: string, number, boolean, null, undefined, symbol, bigint. Запомните их количество и названия - это часто спрашивают на собеседованиях.
