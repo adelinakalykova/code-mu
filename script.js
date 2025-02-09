8b4b3e87b5f039311b9a6954f2aad3fe92e04f00
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-09 19:50:06 +0600

    obj keys orfograph

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-//Создайте объект date с ключами 'year', 'month' и 'day' и значениями, соответствующими текущему дню. Выведите созданную дату на экран в формате год-месяц-день.
-let date = {
-  year: "2025",
-  month: "02",
-  day: "10",
+// Исправьте ошибки, допущенные в следующем коде:
+
+let obj = {
+  "1a": 1,
+  b2: 2,
+  "c-c": 3,
+  "d 4": 4,
+  e5: 5,
 };
-console.log(date.year + " " + date.month + " " + date.day);
+
+console.log(obj["1a"]);
+console.log(obj.b2);
+console.log(obj["c-c"]);
+console.log(obj["d 4"]);
+console.log(obj.e5);
