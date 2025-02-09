c7712d652ce4c347059909a0b655be86758d8798
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-09 19:37:14 +0600

    object month

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-// Создайте объект с ключами от 1 до 7, в качестве значений содержащий имена дней недели. Выведите на экран все его элементы.
-let Week = {
-  1: "Monday",
-  2: "Tuesday",
-  3: "Wednesday",
-  4: "Thursday",
-  5: "Friday",
-  6: "Saturday",
-  7: "Sunday",
-};
-
-console.log(Week);
+let months = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December'
+  };
+  
+  console.log(months);  
+  
