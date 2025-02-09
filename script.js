6fcf544e59debeb09f0ebc9c498ffc87c3829d5d
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-09 20:05:53 +0600

    object length

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Получите массив ключей следующего объекта:
+// Найдите количество элементов в следующем объекте:
 
 let obj = {x: 1, y: 2, z: 3};
-let keys = Object.keys(obj);
-console.log(keys);
+console.log(Object.keys(obj).length);
