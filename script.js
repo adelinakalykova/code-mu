bf64c03ff4f4cb5d53fe854978fdf523234728fa
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-09 20:16:36 +0600

    found error in object keys

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // Исправьте ошибку, допущенную в следующем коде:
 
-// let obj = { x: 1, y: 2, z: 3 };
-// console.log(obj["x"]);
+// let obj = {x: 1, y: 2, z: 3};
+
+// let prop = 'x';
+// console.log(obj[prop]);
 
 // Исправьте ошибку, допущенную в следующем коде:
 
 let obj = {x: 1, y: 2, z: 3};
-let key = 'x';
-console.log(obj[key]);
+
+let prop = 'x';
+console.log(obj[prop]);
