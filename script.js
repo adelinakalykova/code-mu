138c848d14fc0f21e8e53dfe6b02d39c1e0c5dfa
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-09 16:19:15 +0600

    number operations

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-//Дана строка '12345'. Найдите сумму цифр этой строки.
-let str = "12345";
-alert(
-  Number(str[0]) +
-    Number(str[1]) +
-    Number(str[2]) +
-    Number(str[3]) +
-    Number(str[4])
-);
+//Дано число 12345. Найдите сумму цифр этого числа.
+// let num = "12345";
+// let str = String(num);
+// alert(
+//   Number(str[0]) +
+//     Number(str[1]) +
+//     Number(str[2]) +
+//     Number(str[3]) +
+//     Number(str[4])
+// );
+
+//Дано число 12345. Найдите произведение цифр этого числа.
+
+// let num = "12345";
+// let str = String(num);
+// alert(
+//   Number(str[0]) *
+//     Number(str[1]) *
+//     Number(str[2]) *
+//     Number(str[3]) *
+//     Number(str[4])
+// );
+
+//Дано число 12345. Переставьте цифры этого числа в обратном порядке.
+let num = 12345;
+let str = num.toString();
+let reversed =
+  str[str.length - 1] +
+  str[str.length - 2] +
+  str[str.length - 3] +
+  str[str.length - 4] +
+  str[str.length - 5];
+alert(reversed);
