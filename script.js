860169b14421485bfc5970d94850a9e4c209a672
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-10 15:11:31 +0600

    reduce log type true

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-//Вспомните и напишите, какие значения при приведении к логическому типу дают false.
-console.log(Boolean(0));          
-console.log(Boolean(-0));         
-console.log(Boolean(+0));        
-console.log(Boolean(null));      
-console.log(Boolean(false));      
-console.log(Boolean(NaN));      
-console.log(Boolean(undefined));  
-console.log(Boolean(''));
+
+
+// Перепишите следующий код с использованием сокращенной формы:
+
+// let test = true;
+
+// if (test === true) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+let test = true;
+
+if (test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
