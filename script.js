ff3643000635c02dd7db4b0835ad329bb569710d
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-10 14:18:19 +0600

    == operation

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
-//Проверьте, что переменная test больше 10.
-// let test = 15;
-// if (test > 10) {
-//   console.log("+");
-// } else {
-//   console.log("-");
-// }
-
-//Проверьте, что переменная test меньше 10.
-// let test = 9;
-// if (test > 10) {
-//   console.log("+");
-// } else {
-//   console.log("-");
-// }
-
-//Проверьте, что переменная test больше или равна 10.
+// Проверьте, что переменная test равна 10.
 // let test = 10;
-// if (test >= 10) {
+// if (test == 10) {
 //   console.log("+");
 // } else {
 //   console.log("-");
 // }
-
-//Проверьте, что переменная test меньше или равна 10.
-let test = 5;
-if (test <= 10) {
-  console.log("+");
-} else {
-  console.log("-");
-}
