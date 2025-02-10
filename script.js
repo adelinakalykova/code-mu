8b36efd61a76f253b683d3712434f47e5c364485
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-10 14:06:05 +0600

    const and let

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //В JavaScript семь примитивных типов данных: string, number, boolean, null, undefined, symbol, bigint. Запомните их количество и названия - это часто спрашивают на собеседованиях.
+const arr = [1, 2, 3, 4, 5];
+const res = arr[1] + arr[2];
+
+console.log(res);
