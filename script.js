77c4e021db3799b3adac84f9941d8bb55298cf2f
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-10 15:28:36 +0600

    reduce conditions

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Перепишите следующий код в сокращенной форме:
+//Перепишите следующий код в сокращенной форме:
 
 // let test1 = true;
 // let test2 = true;
 
-// if (test1 === true && test2 === true) {
+// if (test1 === true && test2 !== true) {
 // 	console.log('+++');
 // } else {
 // 	console.log('---');
@@ -12,7 +12,7 @@
 let test1 = true;
 let test2 = true;
 
-if (test1 && test2) {
+if (test1 && !test2) {
   console.log("+++");
 } else {
   console.log("---");
