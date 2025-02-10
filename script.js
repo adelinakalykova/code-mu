8a99578c2e4e6c8ea19de8c87732dc6ea7700a1f
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-10 14:20:35 +0600

    != operation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Проверьте, что переменная test равна 10.
-// let test = 10;
-// if (test == 10) {
-//   console.log("+");
-// } else {
-//   console.log("-");
-// }
+// Проверьте, что переменная test не равна 10.
+let test = 10;
+if (test != 10) {
+  console.log("+");
+} else {
+  console.log("-");
+}
