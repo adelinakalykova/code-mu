d3839b5bb997aa813212a5be090c40d4ca6c67ee
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-12 13:40:31 +0600

    Nested if-else constructs

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-// В переменной day лежит какое-то число из интервала от 1 до 31. Определите в какую декаду месяца попадает это число (в первую, вторую или третью).
-// let day = 20;
-// if (day >= 1 && day <= 10) {
-//     console.log("First");
-// } else if (day >= 11 && day <= 20) {
-//     console.log("second");
-// } else if (day >= 21 && day <= 31) {
-//     console.log("Тhirth");
-// } else {
-//     console.log("Error");
-// }
+// Пусть в переменной num хранится число. Если это число попадает в диапазон от 10 до 99, то найдите сумму цифр этого числа. Если полученная сумма меньше или равна 9, то выведите сообщение о том, что сумма цифр однозначна, в противном случае выведите сообщение о том, что сумма цифр двухзначна.
+let num = 51;
 
-//Модифицируйте предыдущую задачу так, чтобы, если в переменной day будет не число от 1 до 31, выдавалось сообщение об ошибке.
-let day = 40;
-if (day >= 1 && day <= 10) {
-    console.log("First");
-} else if (day >= 11 && day <= 20) {
-    console.log("second");
-} else if (day >= 21 && day <= 31) {
-    console.log("Тhirth");
+if (num >= 10 && num <= 99) {
+  let str = String(num);
+  let sum = parseInt(str[0]) + parseInt(str[1]);
+
+  console.log(sum);
+
+  if (sum <= 9) {
+    console.log("однозначна");
+  } else {
+    console.log("двузначна");
+  }
 } else {
-    console.log("Error");
+  console.log("error");
 }
-
