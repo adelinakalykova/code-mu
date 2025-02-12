b31b66efa70a7accc0fc0e186075c234312350e5
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-12 14:08:23 +0600

    Variable scope in if-else

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-// Спросите у пользователя, есть ли ему уже 18 лет. Если есть - выведите на экран алерт с текстом для взрослых, а если нет, выведите сообщение о том, что доступ пользователю запрещен.
-let age = confirm("Are you 18 years old?");
-if (age) {
-  alert("Welcome");
+let age = 17;
+let adult;
+if (age >= 18) {
+  adult = true;
 } else {
-  alert("Not for you");
+  adult = false;
 }
+
+console.log(adult);
