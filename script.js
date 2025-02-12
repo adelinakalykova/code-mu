2a16987bce962fe46f5069d849f36457872b812a
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-12 13:49:15 +0600

    reduce if/else construction

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
-//  Перепишите следующий код через switch-case:
+// Перепишите следующий код через тернарный оператор:
 
-let lang = "ru";
+let num = -1;
+let res = num >= 0 ? "1" : "2";
 
-// if (lang == 'ru') {
-// 	console.log('рус');
-// } else if (lang == 'en') {
-// 	console.log('анг');
-// } else if (lang == 'de') {
-// 	console.log('нем');
-// } else {
-// 	console.log('язык не поддерживается');
-// }
-
-switch (lang) {
-  case "ru":
-    console.log("ru");
-    break;
-  case "en":
-    console.log("en");
-    break;
-  case "de":
-    console.log("de");
-    break;
-  default:
-    console.log("not language");
-}
+console.log(res);
