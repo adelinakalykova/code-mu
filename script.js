61280fd346707d99c5fccece853f718cc96682f0
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-12 13:33:20 +0600

    else if

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-//Перепишите следующий код в сокращенной форме:
-let test = 10;
-if (test > 0) console.log("+++");
-else console.log("---");
+// В переменной day лежит какое-то число из интервала от 1 до 31. Определите в какую декаду месяца попадает это число (в первую, вторую или третью).
+// let day = 20;
+// if (day >= 1 && day <= 10) {
+//     console.log("First");
+// } else if (day >= 11 && day <= 20) {
+//     console.log("second");
+// } else if (day >= 21 && day <= 31) {
+//     console.log("Тhirth");
+// } else {
+//     console.log("Error");
+// }
+
+//Модифицируйте предыдущую задачу так, чтобы, если в переменной day будет не число от 1 до 31, выдавалось сообщение об ошибке.
+let day = 40;
+if (day >= 1 && day <= 10) {
+    console.log("First");
+} else if (day >= 11 && day <= 20) {
+    console.log("second");
+} else if (day >= 21 && day <= 31) {
+    console.log("Тhirth");
+} else {
+    console.log("Error");
+}
+
