05700add43945e138d9af9d1d2bfa4450da51b82
Adelina Kalykova <adelinakalyk@gmail.com>
2025-02-14 14:31:03 +0600

    practicum for if/else

--- script.js
+++ script.js
@@ -1,43 +1,48 @@
-//Код должен проверить сумму чисел:
+//В переменной month лежит какое-то число из интервала от 1 до 12. Определите в какую пору года попадает этот месяц (зима, лето, весна, осень).
+let month = 5;
+if (month >= 12 && month <= 2) {
+  console.log("Winter");
+} else if (month >= 3 && month <= 5) {
+  console.log("spring");
+} else if (month >= 6 && month <= 8) {
+  console.log("summer");
+} else if (month >= 9 && month <= 11) {
+  console.log("autumn");
+} else {
+  console.log("error");
+}
 
-// let num1 = 1;
-// let num2 = 2;
-// let sum = num1 + num2;
+//Дана строка, состоящая из символов, например, 'abcde'. Проверьте, что первым символом этой строки является буква 'a'. Если это так - выведите 'да', в противном случае выведите 'нет'.
+// let str = "abcde";
+// if ((str[0] = "a")) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
 
-// if (sum === 3) {
-//   console.log(sum);
+//Дано число, например, 12345. Проверьте, что первым символом этого числа является цифра 1, 2 или 3. Если это так - выведите 'да', в противном случае выведите 'нет'.
+// let num = 22345;
+// let str = String(num);
+// if ((str[0] == 1 || 2 || 3)) {
+//   console.log("+");
 // } else {
 //   console.log("-");
 // }
 
-//Код должен проверить сумму чисел:
+//Дано трехзначное число. Найдите сумму цифр этого числа.
+// let numb = 123;
+// let str = String(numb);
+// let sum = +str[0] + +str[1] + +str[2];
+// console.log(sum);
 
-// let num1 = "1";
-// let num2 = "2";
-// let sum = +num1 + +num2;
-// if (sum === 3) {
-//   console.log(sum);
+//Дано число из 6-ти цифр. Проверьте, что сумма первых трех цифр равняется сумме вторых трех цифр. Если это так - выведите 'да', в противном случае выведите 'нет'.
+// let num = String(111112);
+// let sum1 = +num[0] + +num[1] + +num[2];
+// let sum2 = +num[3] + +num[4] + +num[5];
+// if (sum1 == sum2) {
+//   console.log("++++");
 // } else {
-//   console.log("---");
+//   console.log("----");
 // }
 
-//Код должен проверить сумму чисел:
-
-// let num1 = "1";
-// let num2 = "2";
-// let sum = Number(num1) + Number(num2);
-// if (sum === 3) {
-//   console.log(sum);
-// } else {
-//   console.log("---");
-// }
-
-//Код должен проверить первую цифру числа:
-
-// let num = 123;
-// let str = String(num);
-// if (str[0] == 1) {
-//   console.log("+++");
-// } else {
-//   console.log("---");
-// }
+//
